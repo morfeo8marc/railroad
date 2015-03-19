a69a6e47f2a3426145fb0f25c8072c586fb2c2ee
morfeo8marci <morfeo8marc@gmail.com>
2015-03-19 07:09:01 +0100

    Initial project configuration

---
+++ src/test/java/com/irontrainsofthegenerality/railroad/RailroadApplicationTests.java
@@ -0,0 +1,18 @@
+package com.irontrainsofthegenerality.railroad;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.irontrainsofthegenerality.railroad.RailroadApplication;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = RailroadApplication.class)
+public class RailroadApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
