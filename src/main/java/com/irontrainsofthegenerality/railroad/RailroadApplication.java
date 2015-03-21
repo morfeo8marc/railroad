5878e10b3902375f8680849f17b92895234ec63d
morfeo8marci <morfeo8marc@gmail.com>
2015-03-21 21:00:21 +0100

    Reomved unnecessary import

--- src/main/java/com/irontrainsofthegenerality/railroad/RailroadApplication.java
+++ src/main/java/com/irontrainsofthegenerality/railroad/RailroadApplication.java
@@ -1,14 +1,7 @@
 package com.irontrainsofthegenerality.railroad;
 
-import java.util.stream.IntStream;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import com.irontrainsofthegenerality.railroad.domain.Town;
-import com.irontrainsofthegenerality.railroad.domain.TownInformation;
-import com.irontrainsofthegenerality.railroad.repository.TownRepository;
 
 @SpringBootApplication
 public class RailroadApplication {
